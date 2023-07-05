b552db9d0d4bf020774d370109519d51284b5d69
Clément Sicard <c.clement.sicard@northvolt.com>
2023-07-05 13:52:01 +0200

    network: updated case + added doc

--- thesaurus/network.py
+++ thesaurus/network.py
@@ -1,5 +1,5 @@
 import random
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
 import networkx as nx
 from loguru import logger
@@ -9,23 +9,27 @@ import thesaurus.consts as consts
 
 
 class Network:
+    """
+    Network class that represents the graph of the UNBIS Thesaurus.
+    """
+
     def __init__(self, verbose: bool = False) -> None:
         self.G = nx.Graph(name="UNBIS Thesaurus")
-        self.clusters = []
+        self.clusters: List[str] = []
 
         self.verbose = verbose
 
-    def add_node(
+    def addNode(
         self,
-        node_id: str,
+        nodeId: str,
         cluster: int,
-        label_en: Optional[str] = None,
-        label_ar: Optional[str] = None,
-        label_es: Optional[str] = None,
-        label_fr: Optional[str] = None,
-        label_ru: Optional[str] = None,
-        label_zh: Optional[str] = None,
-        node_type: str = "topic",
+        labelEn: Optional[str] = None,
+        labelAr: Optional[str] = None,
+        labelEs: Optional[str] = None,
+        labelFr: Optional[str] = None,
+        labelRu: Optional[str] = None,
+        labelZh: Optional[str] = None,
+        nodeType: str = "topic",
     ) -> None:
         """
         Adds a node to the graph. Also saves the node id to the set of node ids.
@@ -33,64 +37,81 @@ class Network:
 
         Parameters
         ----------
-        `node_id` : `str`
+        `nodeId` : `str`
             Unique id of the node, same as in UNBIS Thesaurus
-        `label_en` : `Optional[str]`, optional
-            Label in English, by default None
-        `label_ar` : `Optional[str]`, optional
-            Label in Arabic, by default None
-        `label_es` : `Optional[str]`, optional
-            Label in Spanish, by default None
-        `label_fr` : `Optional[str]`, optional
-            Label in French, by default None
-        `label_ru` : `Optional[str]`, optional
-            Label in Russian, by default None
-        `label_zh` : `Optional[str]`, optional
-            Label in Chinese, by default None
-        `type_` : `str`, optional
-            Type of node, by default "topic". Must be one of "meta_topic", "topic", "subtopic"
+        `cluster` : `int`
+            Cluster id of the node
+        `labelEn` : `Optional[str]`, optional
+            Label in English, by default `None`
+        `labelAr` : `Optional[str]`, optional
+            Label in Arabic, by default `None`
+        `labelEs` : `Optional[str]`, optional
+            Label in Spanish, by default `None`
+        `labelFr` : `Optional[str]`, optional
+            Label in French, by default `None`
+        `labelRu` : `Optional[str]`, optional
+            Label in Russian, by default `None`
+        `labelZh` : `Optional[str]`, optional
+            Label in simplified Chinese, by default `None`
+        `nodeType` : `str`, optional
+            Type of node, by default `"topic"`. Must be one of `"meta_topic"`, `"topic"`, `"subtopic"`
         """
-        assert node_type in consts.NODE_TYPES, f"Invalid type {node_type}"
+
+        assert nodeType in consts.NODE_TYPES, f"Invalid type {nodeType}"
 
         # Save node id to set of node ids
         node_json = {
-            "key": node_id,
+            "key": nodeId,
             "x": (random.random() - 0.5) * 1000,
             "y": (random.random() - 0.5) * 1000,
             "cluster": cluster,
-            "url": consts.BASE_URL.format(node_id),
-            "node_type": node_type,
+            "url": consts.BASE_URL.format(nodeId),
+            "node_type": nodeType,
             "tag": "Concept",
-            "label_en": label_en,
-            "label_ar": label_ar,
-            "label_es": label_es,
-            "label_fr": label_fr,
-            "label_ru": label_ru,
-            "label_zh": label_zh,
+            "label_en": labelEn,
+            "label_ar": labelAr,
+            "label_es": labelEs,
+            "label_fr": labelFr,
+            "label_ru": labelRu,
+            "label_zh": labelZh,
         }
 
-        self.G.add_node(node_id, **node_json)
+        self.G.add_node(nodeId, **node_json)
 
-    def add_edge(
+    def addEdge(
         self,
         source: str,
         target: str,
-        edge_type: str = "topic->subtopic",
+        edgeType: str = "topic->subtopic",
     ) -> None:
-        assert edge_type in consts.EDGE_TYPES, f"Invalid edge type {edge_type}"
+        """
+        Adds an edge to the graph. Edge type is defined in `consts.py` and is based on the edge type.
+
+        Parameters
+        ----------
+        `source` : `str`
+            Source node id
+        `target` : `str`
+            Target node id
+        `edgeType` : `str`, optional
+            Edge type, by default `"topic->subtopic"`
+        """
+        assert edgeType in consts.EDGE_TYPES, f"Invalid edge type {edgeType}"
 
         edge_id = (source, target)
 
-        self.G.add_edge(*edge_id, edge_type=edge_type)
+        self.G.add_edge(*edge_id, edge_type=edgeType)
 
-    def to_json(self) -> dict:
+    def toJson(self) -> Dict[str, Any]:
         """
+        Converts the graph to a json format that can be used by the frontend.
+
         Returns
         -------
-        `dict`
-            JSON representation of the graph
+        `Dict[str, Any]`
+            JSON representation of the graph (as a dict here)
         """
-        j = nx.readwrite.json_graph.node_link_data(self.G, link="edges")
+        j: Dict[str, Any] = nx.readwrite.json_graph.node_link_data(self.G, link="edges")
 
         # Replace weird format for edges
         edges = [[e["source"], e["target"]] for e in j["edges"]]
