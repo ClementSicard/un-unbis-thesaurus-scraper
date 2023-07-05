44d2522c5a0540d8f330775f5a6710d1e6a741ab
Clément Sicard <c.clement.sicard@northvolt.com>
2023-07-05 15:49:38 +0200

    consts: adds JSON type

--- thesaurus/consts.py
+++ thesaurus/consts.py
@@ -1,3 +1,7 @@
+from typing import Any, Dict, List
+
+JSON = Dict[str, Any] | List[Dict[str, Any]]
+
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) \
         Chrome/44.0.2403.157 Safari/537.36",
